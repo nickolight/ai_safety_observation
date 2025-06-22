71fcacfbe96a9eaa98375c0862987d861058907d
Mykola Bezkorovainyi <m.bez@intobi.com>
2025-06-22 21:41:11 +0300

    Added dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.nickolight.ai_safety_observation"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
